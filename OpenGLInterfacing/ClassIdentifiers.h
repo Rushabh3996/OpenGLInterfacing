1b540c83a869ad08f7de855b9ac6f741a29cd170
Manisha <patole.manisha66@gmail.com>
2018-01-22 01:27:27 +0530

    Final Sphere Code changes

--- OpenGLInterfacing/ClassIdentifiers.h
+++ OpenGLInterfacing/ClassIdentifiers.h
@@ -2,10 +2,12 @@
 #include "Lights.h"
 #include "Cube.h"
 #include "Pyramid.h"
+#include"Sphere.h"
 
 #define LIGHTS 0
 #define CUBE 1
 #define PYRAMID 2
+#define SPHERE 3
 
-#define NUM_OF_CLASS 3
+#define NUM_OF_CLASS 4
 
